58b3f3139dfa8b3b6a460dbf0cac3efa848542ac
Chris Glick <chrisglick@gmail.com>
2017-10-18 16:03:08 -0700

    populated array, copied shuffle func

--- src/geneticAlgo.js
+++ src/geneticAlgo.js
@@ -1,14 +1,38 @@
 
 
-const generateOneDimensionalSamples = (numSamples) => {
-  const samples = new Array(numSamples)
-  return samples
+const generateOneDimensionalSample = (numSamples) => {
+  const sample = new Array(numSamples)
+  for (i = 1; i <= numSamples; i++) {
+    sample[i- 1] = i
+  }
+  console.log(sample)
+  return sample
 }
 
 
 const basic = () => { return true }
 
+// code not written by me
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+
+//
+
+
 module.exports = {
-  generateOneDimensionalSamples,
+  generateOneDimensionalSample,
   basic,
 }
--- src/geneticAlgo.spec.js
+++ src/geneticAlgo.spec.js
@@ -1,11 +1,24 @@
 const equal = require('assert').deepEqual
-const { generateOneDimensionalSamples, basic } = require('./geneticAlgo.js')
+const notEqual = require('assert').notDeepEqual
+const { generateOneDimensionalSample, basic } = require('./geneticAlgo.js')
 
 describe( 'geneticAlgo.js', () => {
   describe('generateOneDimensionalSamples()', () => {
       it('should return an array with X elements', () => {
-        const result = generateOneDimensionalSamples(9)
+        const result = generateOneDimensionalSample(9)
         equal(result.length, 9)
+      })
+
+      it('should return an array populated with numbers from 1 to num samples + 1', () => {
+        const result = generateOneDimensionalSample(9)
+        const notFound = -1
+        const firstNumLocation = result.indexOf(1)
+        notEqual(firstNumLocation, notFound)
+        const lastNumLocation = result.indexOf(9)
+        notEqual(lastNumLocation, notFound)
+
+        const shouldNotGenerate = result.indexOf(10)
+        equal(shouldNotGenerate, notFound)
       })
   })
 
